ee8da1a8ce72e7c8623e2442d9e89dace8252a62
Nakrit Aree <nakritaree@gmail.com>
2024-03-10 14:45:07 +0700

    First File Commit

---
+++ plus.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int summ(int a,int b){
+    return a + b;
+}
+int main()
+{
+    int a,b;
+    printf("Insert number : ");
+    scanf("%d %d",&a,&b);
+    //scanf("%d",&b);
+
+    printf("The sum of %d and %d is : %d",a,b,summ(a,b));
+    return 0;
+}
